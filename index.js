156e7d1befe7ce21c830b752a042577676c35f31
Jayleen Li <jayleenli17@gmail.com>
2020-11-05 20:15:50 -0800

    Site up

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 //Your JavaScript code will go here!
 window.onload = function() {
+	document.getElementById("create-meme").addEventListener("click", function() {
+		const topText = document.getElementById("top-text-input").value;
+		document.getElementById("top-text").innerText = topText;
 
+		const bottomText = document.getElementById("bottom-text-input").value;
+		document.getElementById("bottom-text").innerText = bottomText;
+
+		const imgSrc = document.getElementById("img-src-input").value;
+		if (imgSrc) {
+			document.getElementById("meme-pic").src = imgSrc;
+		}
+	});
 }
